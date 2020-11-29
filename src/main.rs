973ef6c2650099ae3e66ad609c6a953bae6a1e92
Gijs de Jong <14833076+OxKitsune@users.noreply.github.com>
2020-11-29 14:46:37 +0100

    Fix canonicalisation formatting

--- src/main.rs
+++ src/main.rs
@@ -16,52 +16,53 @@ fn main() {
             exit(1);
         }
     };
-    
-    let path = match std::fs::canonicalize(&args[1]) {
-        Ok(buff) => format!("{:?}", buff),
-        Err(err) => {
-            eprintln!("Failed to get absoulte path: {}", err);
-            exit(1);
-        }
-    };
 
-    println!("Exporting {:?}...", path);
-    let file_open = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(format!("{}/.zshrc", home_dir));
+    match std::fs::canonicalize(&args[1]) {
+        Ok(buffer) => {
+            let path = buffer.to_str().unwrap().clone();
+            println!("Exporting {}...", path);
+            let file_open = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(format!("{}/.zshrc", home_dir));
 
-    match file_open {
-        Ok(mut file) => {
-            let command = &*format!("export PATH=\"{}:$PATH\"", path);
-            let result = writeln!(file, "\n{}", command);
+            match file_open {
+                Ok(mut file) => {
+                    let command = &*format!("export PATH=\"{}:$PATH\"", path);
+                    let result = writeln!(file, "\n{}", command);
 
-            match result {
-                Ok(()) => {
-                    println!("Added {} to path!", path);
+                    match result {
+                        Ok(()) => {
+                            println!("Added {} to path!", path);
 
-                    let ses_command = Command::new("sh").arg("-c").arg(command).output();
+                            let ses_command = Command::new("sh").arg("-c").arg(command).output();
 
-                    match ses_command {
-                        Ok(_) => {
-                            println!("Added {} to current session!", path);
-                            exit(0);
+                            match ses_command {
+                                Ok(_) => {
+                                    println!("Added {} to current session!", path);
+                                    exit(0);
+                                }
+                                Err(err) => {
+                                    eprintln!("Failed to add {} to curent session: {}", path, err);
+                                    exit(1);
+                                }
+                            }
                         }
                         Err(err) => {
-                            eprintln!("Failed to add {} to curent session: {}", path, err);
+                            eprintln!("Failed to write to .zshrc: {}", err);
                             exit(1);
                         }
                     }
                 }
                 Err(err) => {
-                    eprintln!("Failed to write to .zshrc: {}", err);
+                    eprintln!("Failed to open .zshrc: {}", err);
                     exit(1);
                 }
             }
         }
         Err(err) => {
-            eprintln!("Failed to open .zshrc: {}", err);
+            eprintln!("Failed to get absoulte path: {}", err);
             exit(1);
         }
-    }
+    };
 }
